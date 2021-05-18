a4a057010865c546a21defc69352f98f652e2d41
wangfan <fan.wang@conflux-chain.org>
2021-05-18 18:03:00 +0800

    add params

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ node swap.js --token ${token_hex_address} --amountBnbIn ${bnb_to_swap} --amountO
 EXAMPLE
 
 ```bash
-node swap.js --token 0xe9e7cea3dedca5984780bafc599bd69add087d56  --amountBnbIn 1  --amountOutMin 500 --privatekey 0x...
+node swap.js --token 0xe9e7cea3dedca5984780bafc599bd69add087d56  --amountBnbIn 1  --amountOutMin 500 --maxGasPrice 10 --gasPriceStep 8 --resendTimeout 5 --privatekey 0x...
 ```
 
-This will swap exact 1 bnb to at least 500 bUSD.
+This will swap exact 1 bnb to at least 500 bUSD. If the sent transaction is not packed in 5 seconds, it will send a new transaction with gas price of 8% percent higher. The gas price upper bound is 10 GWei.
--- src/swap.js
+++ src/swap.js
@@ -13,6 +13,20 @@ const wbnbAddress = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
 
 const router = new w3.eth.Contract(routerAbi, routerAddress);
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+async function printReceipt(hashes) {
+  for (let i = 0; i < hashes.length; ++i) {
+    let receipt = await w3.eth.getTransactionReceipt(hashes[i]);
+    if (receipt !== null) {
+      console.log(receipt);
+      return;
+    }
+  }
+}
+
 async function run() {
   let nonce = await w3.eth.getTransactionCount(account.address);
   let decimals = await token.methods.decimals().call();
@@ -23,6 +37,7 @@ async function run() {
   let deadline = Math.floor(Date.now() / 1000 + 1000);
   let gasPrice = new BigNumber(await w3.eth.getGasPrice())
     .multipliedBy(1.1)
+    .integerValue()
     .toString(10);
   let tx_params = {
     from: account.address,
@@ -36,26 +51,59 @@ async function run() {
   };
   try {
     let gas = new BigNumber(await w3.eth.estimateGas(tx_params));
-    tx_params.gas = gas.multipliedBy(1.3).toString(10);
+    tx_params.gas = gas.multipliedBy(1.3).integerValue().toString(10);
   } catch (e) {
     console.log(e);
     console.log(`transaction will always fail. exit.`);
     return;
   }
-  try {
-    const { rawTransaction } = await w3.eth.accounts.signTransaction(
-      tx_params,
-      account.privateKey,
-    );
-    let event = this.w3.eth.sendSignedTransaction(rawTransaction);
-    let hash = await new Promise((resolve, reject) => {
-      event.once('transactionHash', resolve);
-      event.once('error', reject);
-    });
-    event.catch(() => {});
-    console.log(`transaction broadcast with bsc hash: ${hash}`);
-  } catch (e) {
-    console.log(e);
+  let txHashes = [];
+  for (;;) {
+    try {
+      const { rawTransaction } = await w3.eth.accounts.signTransaction(
+        tx_params,
+        account.privateKey,
+      );
+      let event = this.w3.eth.sendSignedTransaction(rawTransaction);
+      let hash = await new Promise((resolve, reject) => {
+        event.once('transactionHash', resolve);
+        event.once('error', reject);
+      });
+      txHashes.push(hash);
+      event.catch(() => {});
+      console.log(`transaction broadcast with bsc hash: ${hash}`);
+      // check nonce
+      let start = Date.now();
+      for (;;) {
+        await sleep(500);
+        let now = Date.now();
+        let curNonce = await w3.eth.getTransactionCount(account.address);
+        if (curNonce > nonce) {
+          console.log(`transaction packed.`);
+          printReceipt(txHashes);
+          return;
+        }
+        if ((now - start) / 1000 > resendTimeout) break;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    tx_params.gas = new BigNumber(tx_params.gas)
+      .multipliedBy(100 + program.gasPriceStep)
+      .divided(100);
+    if (tx_params.gas.isLargerThan(maxGasPrice)) {
+      console.log(`gas price reached upper bound.`);
+      for (;;) {
+        await sleep(500);
+        let curNonce = await w3.eth.getTransactionCount(account.address);
+        if (curNonce > nonce) {
+          console.log(`transaction packed.`);
+          printReceipt(txHashes);
+          return;
+        }
+      }
+    }
+    tx_params.gas = tx_params.gas.integerValue().toString(10);
   }
 }
 
@@ -64,6 +112,9 @@ program
   .option('-n, --amountBnbIn [type]', 'bnb amount')
   .option('-m, --amountOutMin [type]', 'minimal out value')
   .option('-p, --privatekey [type]', 'private key of owner')
+  .option('-g0, --maxGasPrice [type]', 'max gas price')
+  .option('-g1, --gasPriceStep [type]', 'gas price step')
+  .option('-r, --resendTimeout [type]', 'resend timeout')
   .parse(process.argv);
 
 let token = new w3.eth.Contract(erc20Abi, program.token);
@@ -72,5 +123,8 @@ let amountBnbIn = new BigNumber(program.amountBnbIn)
   .multipliedBy(1e18)
   .toString(10);
 let account = w3.eth.accounts.privateKeyToAccount(program.privatekey);
+let maxGasPrice = new BigNumber(program.maxGasPrice).multipliedBy(1e9);
+let gasPriceStep = new BigNumber(program.gasPriceStep);
+let resendTimeout = program.resendTimeout;
 
 run();
